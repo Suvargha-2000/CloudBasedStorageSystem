8857f28c3023a2959fedfa23bc85326efdf7e23a
Suvargha <suvargha2000@gmail.com>
2023-04-05 01:55:48 +0530

    Adding User Auth Urls

--- BackEnd/cloudBasedStorageSystem/userAuthentication/urls.py
+++ BackEnd/cloudBasedStorageSystem/userAuthentication/urls.py
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('' , views.apiAuthentication.as_view(), name ='hello'),
     path('createUser' , views.createUser.as_view() , name="create_user_details"),
+    path('deleteUser' , views.deleteUser.as_view() , name="delete_user_details"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
